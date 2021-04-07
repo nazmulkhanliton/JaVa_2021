a0b1225e957fe75f57064da56797f5837d086705
Nazmul Khan Liton <41168894+nazmulkhanliton@users.noreply.github.com>
2021-04-08 03:17:55 +0600

    Add files via upload
    1

---
+++ Object_Example.java
@@ -0,0 +1,10 @@
+public class Object_Example {
+    public void Puppy(String name){
+        System.out.println("Name" + name);
+    }
+
+    public static void main(String[] args) {
+        Object_Example object_example = new Object_Example();
+        object_example.Puppy("Tommy");
+    }
+}
